1805dfb205ef99f425cd7e73cd916b42f6557f8d
adamcee <adam.cahan@gmail.com>
2021-05-17 16:50:42 -0500

    3 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -2,13 +2,24 @@ import { Component } from '@angular/core';
 
 import { products } from '../products';
 
+console.log('products:');
+console.log(JSON.stringify(products));
+
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
+
+const myObject = {
+  myProperty: 'my property on myObject',
+};
+
+console.log(myObject.myProperty);
+
 export class ProductListComponent {
   products = products;
+  myProperty = 'my property on ProductListComponent';
 
   share() {
     window.alert('The product has been shared!');
