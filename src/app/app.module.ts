9c7569623941197b125502829e3d45ca619b26c0
adamcee <adam.cahan@gmail.com>
2021-05-18 11:25:54 -0500

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,20 +12,13 @@ import { ProductListComponent } from './product-list/product-list.component';
     BrowserModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
+      { path: '', component: ProductListComponent }
     ])
   ],
-  declarations: [
-    AppComponent,
-    TopBarComponent,
-    ProductListComponent
-  ],
-  bootstrap: [
-    AppComponent
-  ]
+  declarations: [AppComponent, TopBarComponent, ProductListComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
-
+export class AppModule {}
 
 /*
 Copyright Google LLC. All Rights Reserved.
