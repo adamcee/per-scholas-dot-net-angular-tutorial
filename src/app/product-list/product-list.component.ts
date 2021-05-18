9a91724e9d50ec0fbb903cf4d53e62acb34aae9b
adamcee <adam.cahan@gmail.com>
2021-05-18 11:03:34 -0500

    2 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -2,21 +2,11 @@ import { Component } from '@angular/core';
 
 import { products } from '../products';
 
-console.log('products:');
-console.log(JSON.stringify(products));
-
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-
-const myObject = {
-  myProperty: 'my property on myObject',
-};
-
-console.log(myObject.myProperty);
-
 export class ProductListComponent {
   products = products;
   myProperty = 'my property on ProductListComponent';
